3e569af04ff906046d0d54464df68ee967edc4e8
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2019-09-16 21:36:57 +0300

    Add roundtrip complex test for XML
    Almost copy of JSON one

--- tests/CommonSecretPlainXMLTests.cs
+++ tests/CommonSecretPlainXMLTests.cs
@@ -75,5 +75,65 @@ namespace Tests
 			Assert.AreEqual(filename1, System.Text.Encoding.UTF8.GetString(cscDeserialized.files[0].filename));
 			CollectionAssert.AreEqual(file1Content, cscDeserialized.files[0].fileContent);
 		}
+
+		[Test]
+		public void RoundTripComplexTest()
+		{
+			// Arrange
+			CommonSecretsContainer csc = new CommonSecretsContainer();
+
+			string password = "dragon667";
+			byte[] initialCounter1 = new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+			SettingsAES_CTR settingsAES_CTR1 = new SettingsAES_CTR(initialCounter1);
+			SymmetricKeyAlgorithm skaAES = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 256, settingsAES_CTR1);
+
+			KeyDerivationFunctionEntry kdfe = KeyDerivationFunctionEntry.CreateHMACSHA256KeyDerivationFunctionEntry("does not matter");
+
+			int notesAmount = 17;
+			int notesSecretAmount = 11;
+
+			// Act
+			byte[] derivedPassword = kdfe.GeneratePasswordBytes(password);
+
+			for (int i = 0; i < notesAmount; i++)
+			{
+				csc.notes.Add(ContentGenerator.GenerateRandomNote());
+			}
+
+			for (int i = 0; i < notesSecretAmount; i++)
+			{
+				csc.noteSecrets.Add(new NoteSecret(ContentGenerator.GenerateRandomNote(), skaAES, derivedPassword));
+			}
+
+			string xml = null;
+			var xmlserializer = new XmlSerializer(typeof(CommonSecretsContainer));
+			var stringWriter = new StringWriter();
+			using (var writer = XmlWriter.Create(stringWriter))
+			{
+				xmlserializer.Serialize(writer, csc);
+				xml = stringWriter.ToString();
+			}
+
+			CommonSecretsContainer cscDeserialized = null;
+			using (XmlReader reader = XmlReader.Create(new StringReader(xml)))
+			{
+				cscDeserialized = (CommonSecretsContainer)xmlserializer.Deserialize(reader);
+			}
+
+			// Assert
+			Assert.AreEqual(notesAmount, csc.notes.Count);
+			Assert.AreEqual(notesAmount, cscDeserialized.notes.Count);
+			for (int i = 0; i < notesAmount; i++)
+			{
+				Assert.IsTrue(ComparisonHelper.AreNotesEqual(csc.notes[i], cscDeserialized.notes[i]));
+			}
+
+			Assert.AreEqual(notesSecretAmount, csc.noteSecrets.Count);
+			Assert.AreEqual(notesSecretAmount, cscDeserialized.noteSecrets.Count);
+			for (int i = 0; i < notesSecretAmount; i++)
+			{
+				Assert.IsTrue(ComparisonHelper.AreNotesSecretEqual(csc.noteSecrets[i], cscDeserialized.noteSecrets[i]));
+			}
+		}
 	}
 }
