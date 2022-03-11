c419a109ba9431c4b4abceda17749b541e63c61f
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-03-11 21:27:28 +0200

    Add async KeyDerivationFunctionEntry
    With two test cases, still WIP

---
+++ src/KeyDerivationFunctionEntryAsync.cs
@@ -0,0 +1,164 @@
+#if ASYNC_WITH_CUSTOM
+// This file has async methods
+
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Threading.Tasks;
+
+namespace CSCommonSecrets
+{
+	/// <summary>
+	/// Class that stores all needed info generating derived passwords from plain text passwords
+	/// </summary>
+	public sealed partial class KeyDerivationFunctionEntry
+	{
+		/// <summary>
+		/// Private constructor for Key Derivation Function Entry, please use CreateKeyDerivationFunctionEntryAsync!
+		/// </summary>
+		/// <param name="prf">KeyDerivationPrf to use</param>
+		/// <param name="saltBytes">Salt byte array</param>
+		/// <param name="iterationsCount">How many iterations</param>
+		/// <param name="howManyBytesAreWanted">How many output bytes are wanted</param>
+		/// <param name="id">Key identifier</param>
+		private KeyDerivationFunctionEntry(KeyDerivationPrf prf, byte[] saltBytes, int iterationsCount, int howManyBytesAreWanted, string id)
+		{
+			// Block SHA-1
+			if (prf == KeyDerivationPrf.HMACSHA1)
+			{
+				throw new ArgumentException($"{nameof(prf)} cannot be SHA1 for security reasons");
+			}
+
+			// Check salt bytes
+			if (saltBytes == null)
+			{
+				throw new ArgumentNullException(nameof(saltBytes));
+			}
+			else if (saltBytes.Length < saltMinLengthInBytes)
+			{
+				throw new ArgumentException($"{nameof(saltBytes)} should be at least {saltMinLengthInBytes} bytes!");
+			}
+
+			// Check iterations count
+			if (iterationsCount < iterationsMin)
+			{
+				throw new ArgumentException($"{nameof(iterationsCount)} should be at least {iterationsMin}!");
+			}
+
+			// Check ID
+			if (string.IsNullOrEmpty(id))
+			{
+				throw new ArgumentException($"{nameof(id)} should contain something!");
+			}
+
+			this.algorithm = KDFAlgorithm.PBKDF2.ToString();
+
+			this.pseudorandomFunction = prf.ToString();
+			
+			this.salt = saltBytes;
+
+			this.iterations = iterationsCount;
+
+			this.derivedKeyLengthInBytes = howManyBytesAreWanted;
+
+			this.keyIdentifier = Encoding.UTF8.GetBytes(id);
+		}
+
+		/// <summary>
+		/// Construct new KeyDerivationFunctionEntry, async
+		/// </summary>
+		/// <param name="prf">KeyDerivationPrf to use</param>
+		/// <param name="saltBytes">Salt byte array</param>
+		/// <param name="iterationsCount">How many iterations</param>
+		/// <param name="howManyBytesAreWanted">How many output bytes are wanted</param>
+		/// <param name="id">Key identifier</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns></returns>
+		public static async Task<KeyDerivationFunctionEntry> CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf prf, byte[] saltBytes, int iterationsCount, int howManyBytesAreWanted, string id, ISecurityAsyncFunctions securityFunctions)
+		{
+			KeyDerivationFunctionEntry keyDerivationFunctionEntry = new KeyDerivationFunctionEntry(prf, saltBytes, iterationsCount, howManyBytesAreWanted, id);
+			// Calculate new checksum
+			await keyDerivationFunctionEntry.CalculateAndUpdateChecksum(securityFunctions);
+			return keyDerivationFunctionEntry;
+		}
+
+		/// <summary>
+		/// Generate derived password
+		/// </summary>
+		/// <param name="regularPassword">"Normal" plaintext password</param>
+		/// <returns></returns>
+		public byte[] GeneratePasswordBytes(string regularPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			Enum.TryParse(this.pseudorandomFunction, out KeyDerivationPrf keyDerivationPrf);
+
+			return KeyDerivation.Pbkdf2(regularPassword, this.salt, keyDerivationPrf, this.iterations, this.derivedKeyLengthInBytes);
+		}
+
+		#region Checksum
+
+		private async Task<string> CalculateHexChecksum(ISecurityAsyncFunctions securityFunctions)
+		{
+			return await ChecksumHelper.CalculateHexChecksumAsync(securityFunctions, Encoding.UTF8.GetBytes(this.algorithm), Encoding.UTF8.GetBytes(this.pseudorandomFunction), this.salt,
+														BitConverter.GetBytes(this.iterations), BitConverter.GetBytes(this.derivedKeyLengthInBytes), this.keyIdentifier);
+		}
+
+		private async Task CalculateAndUpdateChecksum(ISecurityAsyncFunctions securityFunctions)
+		{
+			this.checksum = await this.CalculateHexChecksum(securityFunctions);
+		}
+
+		#endregion // Checksum
+
+
+		#region Static helpers
+
+		/// <summary>
+		/// Create HMACSHA256 based KeyDerivationFunctionEntry with random salt, async
+		/// </summary>
+		/// <param name="id">Key identifier of this entry</param>
+		/// <returns>KeyDerivationFunctionEntry</returns>
+		public static async Task<KeyDerivationFunctionEntry> CreateHMACSHA256KeyDerivationFunctionEntry(string id, ISecurityAsyncFunctions securityFunctions)
+		{
+			int iterationsToDo = suggestedMinIterationsCount;
+			byte[] salt = new byte[saltMinLengthInBytes];
+			
+			// First add some iterations
+			byte[] fourBytes = new byte[4];
+			securityFunctions.GenerateSecureRandomBytes(fourBytes);
+			iterationsToDo += (int)(BitConverter.ToUInt32(fourBytes, 0) % 4096);
+
+			// Then fill salt
+			securityFunctions.GenerateSecureRandomBytes(salt);
+
+			int neededBytes = 32;
+			return await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, salt, iterationsToDo, neededBytes, id, securityFunctions);
+		}
+
+		/// <summary>
+		/// Create HMACSHA512 based KeyDerivationFunctionEntry with random salt, async
+		/// </summary>
+		/// <param name="id">Key identifier of this entry</param>
+		/// <returns>KeyDerivationFunctionEntry</returns>
+		public static async Task<KeyDerivationFunctionEntry> CreateHMACSHA512KeyDerivationFunctionEntry(string id, ISecurityAsyncFunctions securityFunctions)
+		{
+			int iterationsToDo = suggestedMinIterationsCount;
+			byte[] salt = new byte[saltMinLengthInBytes];
+			
+			// First add some iterations
+			byte[] fourBytes = new byte[4];
+			securityFunctions.GenerateSecureRandomBytes(fourBytes);
+			iterationsToDo += (int)(BitConverter.ToUInt32(fourBytes, 0) % 4096);
+
+			// Then fill salt
+			securityFunctions.GenerateSecureRandomBytes(salt);
+
+			int neededBytes = 64;
+			return await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA512, salt, iterationsToDo, neededBytes, id, securityFunctions);
+		}
+
+		#endregion // Static helpers
+	}
+}
+
+#endif // ASYNC_WITH_CUSTOM
---
+++ tests/KeyDerivationFunctionEntryAsyncTests.cs
@@ -0,0 +1,75 @@
+#if ASYNC_WITH_CUSTOM
+
+using NUnit.Framework;
+using CSCommonSecrets;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Text;
+using Newtonsoft.Json;
+
+using System.Threading.Tasks;
+
+namespace Tests
+{
+	public class KeyDerivationFunctionEntryAsyncTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			
+		}
+
+		[Test]
+		public async Task ConstructorAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			KeyDerivationFunctionEntry kdfe1 = new KeyDerivationFunctionEntry();
+
+			KeyDerivationFunctionEntry kdfe2 = await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, new byte[16], 100_000, 32, "master_key", securityAsyncFunctions);
+
+			// Act
+
+			// Assert
+			Assert.IsNotNull(kdfe1);
+			Assert.IsNotNull(kdfe2);
+		}
+
+		[Test]
+		public async Task ConstructorExceptionsASyncTest()
+		{
+			// Arrange
+			byte[] invalidSalt1 = null;
+			byte[] invalidSalt2 = new byte[0];
+			byte[] invalidSalt3 = new byte[15];
+
+			int invalidIterationsCount1 = -100;
+			int invalidIterationsCount2 =  0;
+			int invalidIterationsCount3 = 100;
+
+			string invalidId1 = null;
+			string invalidId2 = "";
+
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			// Act
+
+			// Assert
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA1, new byte[16], 100_000, 32, "master_key", securityAsyncFunctions ));
+
+			Assert.ThrowsAsync<ArgumentNullException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, invalidSalt1, 100_000, 32, "master_key", securityAsyncFunctions ));
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, invalidSalt2, 100_000, 32, "master_key", securityAsyncFunctions ));
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, invalidSalt3, 100_000, 32, "master_key", securityAsyncFunctions ));
+
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, new byte[16], invalidIterationsCount1, 32, "master_key", securityAsyncFunctions ));
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, new byte[16], invalidIterationsCount2, 32, "master_key", securityAsyncFunctions ));
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, new byte[16], invalidIterationsCount3, 32, "master_key", securityAsyncFunctions ));
+
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, new byte[16], 100_000, 32, invalidId1, securityAsyncFunctions ));
+			Assert.ThrowsAsync<ArgumentException>(async () => await KeyDerivationFunctionEntry.CreateKeyDerivationFunctionEntryAsync(KeyDerivationPrf.HMACSHA256, new byte[16], 100_000, 32, invalidId2, securityAsyncFunctions ));
+		}
+	}
+}
+
+#endif // ASYNC_WITH_CUSTOM
