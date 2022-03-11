970e24d137eab92f91cf899fea6c9a252c8557a7
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-03-11 20:57:55 +0200

    Add GenerateNewWithFunctionsAsyncTest test case
    Both async and functions

--- tests/SymmetricKeyAlgorithmAsyncTests.cs
+++ tests/SymmetricKeyAlgorithmAsyncTests.cs
@@ -103,7 +103,7 @@ namespace Tests
 		}
 
 		[Test]
-		public void DeepCopyWitFunctionsTest()
+		public void DeepCopyWithFunctionsTest()
 		{
 			// Arrange
 			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
@@ -125,6 +125,46 @@ namespace Tests
 			CollectionAssert.AreEqual(skaChaCha20.settingsChaCha20.nonce, skaChaCha20Copy.settingsChaCha20.nonce);
 			Assert.AreEqual(skaChaCha20.settingsChaCha20.counter, skaChaCha20Copy.settingsChaCha20.counter);
 			Assert.IsNull(skaChaCha20.settingsAES_CTR);
+		}
+
+		[Test]
+		public async Task GenerateNewWithFunctionsAsyncTest()
+		{
+			// Arrange
+			byte[] keyAES = new byte[16] { 0x2b, 0x7e, 0x12, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0x17, 0x15, 0x88, 0x09, 0xcf, 0x43, 0x3c };
+			byte[] keyChaCha20 = new byte[validChaCha20KeyLength] { 
+														0x07, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
+														0x08, 0x59, 0x0a, 0x0b, 0x2c, 0x0d, 0x0e, 0x0f, 
+														0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 
+														0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f 
+													};
+			byte[] content = new byte[] { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
+
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+			SymmetricKeyAlgorithm skaAES = SymmetricKeyAlgorithm.GenerateNew(SymmetricEncryptionAlgorithm.AES_CTR, securityAsyncFunctions);
+			SymmetricKeyAlgorithm skaChaCha20 = SymmetricKeyAlgorithm.GenerateNew(SymmetricEncryptionAlgorithm.ChaCha20, securityAsyncFunctions);
+
+			// Act
+			byte[] outputAES = await skaAES.EncryptBytesAsync(content, keyAES, securityAsyncFunctions);
+			byte[] outputChaCha20 = await skaChaCha20.EncryptBytesAsync(content, keyChaCha20, securityAsyncFunctions);
+
+			byte[] decryptedAES = await skaAES.EncryptBytesAsync(outputAES, keyAES, securityAsyncFunctions);
+			byte[] decryptedChaCha20 = await skaChaCha20.EncryptBytesAsync(outputChaCha20, keyChaCha20, securityAsyncFunctions);
+
+			// Assert
+			Assert.IsNotNull(skaAES);
+			Assert.IsNotNull(skaAES.settingsAES_CTR);
+			Assert.IsNull(skaAES.settingsChaCha20);
+
+			Assert.IsNotNull(skaChaCha20);
+			Assert.IsNotNull(skaChaCha20.settingsChaCha20);
+			Assert.IsNull(skaChaCha20.settingsAES_CTR);
+
+			CollectionAssert.AreNotEqual(content, outputAES);
+			CollectionAssert.AreNotEqual(content, outputChaCha20);
+
+			CollectionAssert.AreEqual(content, decryptedAES);
+			CollectionAssert.AreEqual(content, decryptedChaCha20);
 		}
 	
 		[Test]
