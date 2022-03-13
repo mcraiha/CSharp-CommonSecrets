14c3e77145d2155c1bf1c88c243b81613e22e7cb
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-03-13 12:43:13 +0200

    Add GetCreationTimeAsyncTest
    Halfway point

--- tests/FileEntrySecretAsyncTests.cs
+++ tests/FileEntrySecretAsyncTests.cs
@@ -189,11 +189,13 @@ namespace Tests
 			// Assert
 			Assert.AreEqual(fileContent.LongLength, fileContentLength);
 		}
-		/*
+		
 		[Test]
-		public void GetCreationTimeTest()
+		public async Task GetCreationTimeAsyncTest()
 		{
 			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
 			byte[] derivedKey = new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 255 };
 			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
 
@@ -204,17 +206,17 @@ namespace Tests
 			string filename = "nice.pdf";
 			byte[] fileContent = new byte[] { 1, 2, 3, 1, 2, byte.MaxValue, 0, 0, 0, 0, 0, 0};
 
-			FileEntry fe = new FileEntry(filename, fileContent);
+			FileEntry fe = await FileEntry.CreateFileEntryAsync(filename, fileContent, securityAsyncFunctions);
 
-			FileEntrySecret fes = new FileEntrySecret(fe, "does not matter", skaAES_CTR, derivedKey);
+			FileEntrySecret fes = await FileEntrySecret.CreateFileEntrySecretAsync(fe, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
 
 			// Act
-			DateTimeOffset fileEntryCreationTime = fes.GetCreationTime(derivedKey);
+			DateTimeOffset fileEntryCreationTime = await fes.GetCreationTimeAsync(derivedKey, securityAsyncFunctions);
 
 			// Assert
 			Assert.AreEqual(fe.GetCreationTime(), fileEntryCreationTime);
 		}
-
+		/*
 		[Test]
 		public void GetModificationTimeTest()
 		{
