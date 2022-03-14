cdb32f670f753488840a912738999373eed2829c
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-03-14 20:01:22 +0200

    Add NoteSecretAsync
    Includes all test cases for that

--- src/NoteSecret/NoteSecretAsync.cs
+++ src/NoteSecret/NoteSecretAsync.cs
@@ -13,8 +13,75 @@ namespace CSCommonSecrets
 	/// </summary>
 	public sealed partial class NoteSecret
 	{
+		/// <summary>
+		/// Create NoteSecret, async
+		/// </summary>
+		/// <param name="note">Note</param>
+		/// <param name="keyIdentifier">Key identifier</param>
+		/// <param name="algorithm">Symmetric Key Algorithm</param>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>NoteSecret</returns>
+		public static async Task<NoteSecret> CreateNoteSecretAsync(Note note, string keyIdentifier, SymmetricKeyAlgorithm algorithm, byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			Dictionary<string, object> dictionaryForAUDALF = new Dictionary<string, object>()
+			{
+				{ Note.noteTitleKey, note.GetNoteTitle() },
+				{ Note.noteTextKey, note.GetNoteText() },
+				{ Note.creationTimeKey, DateTimeOffset.FromUnixTimeSeconds(note.creationTime) },
+				{ Note.modificationTimeKey, DateTimeOffset.FromUnixTimeSeconds(note.modificationTime) },
+			};
+
+			return await CreateNoteSecretAsync(dictionaryForAUDALF, keyIdentifier, algorithm, derivedPassword, securityFunctions);
+		}
+
+		/// <summary>
+		/// Create NoteSecret, async
+		/// </summary>
+		/// <param name="noteAsDictionary">Note as dictionary</param>
+		/// <param name="keyIdentifier">Key identifier</param>
+		/// <param name="algorithm">Symmetric Key Algorithm</param>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>NoteSecret</returns>
+		public static async Task<NoteSecret> CreateNoteSecretAsync(Dictionary<string, object> noteAsDictionary, string keyIdentifier, SymmetricKeyAlgorithm algorithm, byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			NoteSecret noteSecret = new NoteSecret();
+
+			noteSecret.keyIdentifier = Encoding.UTF8.GetBytes(keyIdentifier);
+
+			noteSecret.algorithm = algorithm;
+
+			// Create AUDALF payload from dictionary
+			byte[] serializedBytes = AUDALF_Serialize.Serialize(noteAsDictionary, valueTypes: null, serializationSettings: serializationSettings );
+
+			// Encrypt the AUDALF payload with given algorithm
+			noteSecret.audalfData = await algorithm.EncryptBytesAsync(serializedBytes, derivedPassword, securityFunctions);
+
+			// Calculate new checksum
+			await noteSecret.CalculateAndUpdateChecksumAsync(securityFunctions);
+
+			return noteSecret;
+		}
 
 		#region Common getters
+
+		/// <summary>
+		/// Get Note. Use this for situation where you want to convert secret -> non secret, async
+		/// </summary>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>Note</returns>
+		public async Task<Note> GetNoteAsync(byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			Dictionary<string, object> dict = await this.GetNoteAsDictionaryAsync(derivedPassword, securityFunctions);
+			Note returnValue = await Note.CreateNoteAsync((string)dict[Note.noteTitleKey], (string)dict[Note.noteTextKey], securityFunctions);
+
+			returnValue.creationTime = ((DateTimeOffset)dict[LoginInformation.creationTimeKey]).ToUnixTimeSeconds();
+			returnValue.modificationTime = ((DateTimeOffset)dict[LoginInformation.modificationTimeKey]).ToUnixTimeSeconds();
+
+			return returnValue;
+		}
 
 		/// <summary>
 		/// Get note title, async
@@ -27,11 +94,149 @@ namespace CSCommonSecrets
 			return (string)await Helpers.GetSingleValueAsync(this.audalfData, this.algorithm, derivedPassword, Note.noteTitleKey, deserializationSettings, securityFunctions);
 		}
 
+		/// <summary>
+		/// Get note text, async
+		/// </summary>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>Note text</returns>
+		public async Task<string> GetNoteTextAsync(byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			return (string)await Helpers.GetSingleValueAsync(this.audalfData, this.algorithm, derivedPassword, Note.noteTextKey, deserializationSettings, securityFunctions);
+		}
+
+		/// <summary>
+		/// Get note creation time, async
+		/// </summary>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>Note creation time</returns>
+		public async Task<DateTimeOffset> GetCreationTimeAsync(byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			return (DateTimeOffset)await Helpers.GetSingleValueAsync(this.audalfData, this.algorithm, derivedPassword, Note.creationTimeKey, deserializationSettings, securityFunctions);
+		}
+
+		/// <summary>
+		/// Get note modification time, async
+		/// </summary>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>Note modification time</returns>
+		public async Task<DateTimeOffset> GetModificationTimeAsync(byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			return (DateTimeOffset)await Helpers.GetSingleValueAsync(this.audalfData, this.algorithm, derivedPassword, Note.modificationTimeKey, deserializationSettings, securityFunctions);
+		}
+
+		private async Task<Dictionary<string, object>> GetNoteAsDictionaryAsync(byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			var passwordCheck = Helpers.CheckDerivedPassword(derivedPassword);
+
+			if (!passwordCheck.valid)
+			{
+				throw passwordCheck.exception;
+			}
+
+			// Try to decrypt the binary
+			byte[] decryptedAUDALF = await algorithm.DecryptBytesAsync(this.audalfData, derivedPassword, securityFunctions);
+
+			var audalfCheck = Helpers.CheckAUDALFbytes(decryptedAUDALF);
+
+			if (!audalfCheck.valid)
+			{
+				throw audalfCheck.exception;
+			}
+
+			Dictionary<string, object> noteAsDictionary = AUDALF_Deserialize.Deserialize<string, object>(decryptedAUDALF, settings: deserializationSettings);
+
+			return noteAsDictionary;
+		}
+
+		/// <summary>
+		/// Can the content be decrypted with given derived password, async
+		/// </summary>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>True if can be; False otherwise</returns>
+		public async Task<bool> CanBeDecryptedWithDerivedPasswordAsync(byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			var passwordCheck = Helpers.CheckDerivedPassword(derivedPassword);
+
+			if (!passwordCheck.valid)
+			{
+				return false;
+			}
+
+			// Try to decrypt the binary
+			byte[] decryptedAUDALF = await algorithm.DecryptBytesAsync(this.audalfData, derivedPassword, securityFunctions);
+
+			var audalfCheck = Helpers.CheckAUDALFbytes(decryptedAUDALF);
+
+			if (!audalfCheck.valid)
+			{
+				return false;
+			}
+
+			return true;
+		}
+
 		#endregion // Common getters
 
 
 		#region Common setters
 
+		/// <summary>
+		/// Set note title, async
+		/// </summary>
+		/// <param name="newNoteTitle">New title</param>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>True if set was success; False otherwise</returns>
+		public async Task<bool> SetNoteTitleAsync(string newNoteTitle, byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			return await this.GenericSet(Note.noteTitleKey, newNoteTitle, DateTimeOffset.UtcNow, derivedPassword, securityFunctions);
+		}
+
+		/// <summary>
+		/// Set note text, async
+		/// </summary>
+		/// <param name="newNoteText">New text</param>
+		/// <param name="derivedPassword">Derived password</param>
+		/// <param name="securityFunctions">Security functions</param>
+		/// <returns>True if set was success; False otherwise</returns>
+		public async Task<bool> SetNoteTextAsync(string newNoteText, byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			return await this.GenericSet(Note.noteTextKey, newNoteText, DateTimeOffset.UtcNow, derivedPassword, securityFunctions);
+		}
+
+		private async Task<bool> GenericSet(string key, object value, DateTimeOffset modificationTime, byte[] derivedPassword, ISecurityAsyncFunctions securityFunctions)
+		{
+			try 
+			{
+				Dictionary<string, object> noteAsDictionary = await this.GetNoteAsDictionaryAsync(derivedPassword, securityFunctions);
+				// Update wanted value
+				noteAsDictionary[key] = value;
+				// Update modification time
+				noteAsDictionary[Note.modificationTimeKey] = modificationTime;
+
+				// Generate new algorithm since data has changed
+				this.algorithm = SymmetricKeyAlgorithm.GenerateNew(this.algorithm.GetSymmetricEncryptionAlgorithm(), securityFunctions);
+
+				// Create AUDALF payload from dictionary
+				byte[] serializedBytes = AUDALF_Serialize.Serialize(noteAsDictionary, valueTypes: null, serializationSettings: serializationSettings );
+
+				// Encrypt the AUDALF payload with given algorithm
+				this.audalfData = await algorithm.EncryptBytesAsync(serializedBytes, derivedPassword, securityFunctions);
+
+				// Calculate new checksum
+				await this.CalculateAndUpdateChecksumAsync(securityFunctions);
+
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 
 		#endregion // Common setters
 
@@ -43,7 +248,7 @@ namespace CSCommonSecrets
 			return await ChecksumHelper.CalculateHexChecksumAsync(securityFunctions, this.keyIdentifier, this.audalfData, algorithm.GetSettingsAsBytes());
 		}
 
-		private async Task CalculateAndUpdateChecksumASync(ISecurityAsyncFunctions securityFunctions)
+		private async Task CalculateAndUpdateChecksumAsync(ISecurityAsyncFunctions securityFunctions)
 		{
 			this.checksum = await this.CalculateHexChecksumAsync(securityFunctions);
 		}
---
+++ tests/NoteSecret/NoteSecretAsyncTests.cs
@@ -0,0 +1,350 @@
+#if ASYNC_WITH_CUSTOM
+
+using NUnit.Framework;
+using CSCommonSecrets;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+using System.Threading.Tasks;
+
+namespace Tests
+{
+	public class NoteSecretAsyncTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			
+		}
+
+		[Test]
+		public async Task ConstructorAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 1, 1, 1, 4, 5, 6, 7, 7, 7, 10, 11, 12, 13, 14, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf1, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			Dictionary<string, object> testDictionary = new Dictionary<string, object>()
+			{
+				{ Note.noteTitleKey, "My shopping list for tomorrow"}
+			};
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(testDictionary, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+
+			// Assert
+			Assert.IsNotNull(noteSecret);
+			Assert.IsNotNull(noteSecret.audalfData);
+		}
+
+		[Test]
+		public async Task DeepCopyAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 111, 222, 31, 4, 5, 6, 1, 82, 93, 102, 11, 12, 13, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xfb, 0xf3, 0xaa, 0xf5, 0xf6, 0xbb, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for holidays";
+			string text = "peace, happiness, freedom and something";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			NoteSecret noteSecretCopy = new NoteSecret(noteSecret);
+			string noteTitle = await noteSecretCopy.GetNoteTitleAsync(derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.IsFalse(string.IsNullOrEmpty(noteTitle));
+			Assert.AreEqual(title, noteTitle);
+			Assert.AreNotSame(noteSecret.audalfData, noteSecretCopy.audalfData, "AUDALF byte arrays should be in different memory locations");
+			CollectionAssert.AreEqual(noteSecret.keyIdentifier, noteSecretCopy.keyIdentifier);
+			Assert.AreNotSame(noteSecret.keyIdentifier, noteSecretCopy.keyIdentifier, "Key identifier byte arrays should be in different memory locations");
+			Assert.AreEqual(noteSecret.checksum, noteSecretCopy.checksum);
+		}
+
+		[Test]
+		public async Task GetNoteAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xfb, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for holidays";
+			string text = "peace, happiness, freedom";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			Note noteCopy = await noteSecret.GetNoteAsync(derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.IsTrue(ComparisonHelper.AreNotesEqual(note, noteCopy));
+			Assert.AreEqual(note.creationTime, noteCopy.creationTime);
+			Assert.AreEqual(note.modificationTime, noteCopy.modificationTime);
+		}
+
+
+		[Test]
+		public async Task GetNoteTitleAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xfb, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for holidays";
+			string text = "peace, happiness, freedom";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			string noteTitle = await noteSecret.GetNoteTitleAsync(derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.AreEqual(title, noteTitle);
+		}
+
+		[Test]
+		public async Task GetNoteTextAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 15, 200, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xfb, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xf3, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for holidays";
+			string text = "peace, happiness, freedom";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			string noteText = await noteSecret.GetNoteTextAsync(derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.AreEqual(text, noteText);
+		}
+
+		[Test]
+		public async Task GetModificationTimeAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 15, 200, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xfb, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xf3, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for holidays";
+			string text = "peace, happiness, freedom";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+			
+			// Act
+			DateTimeOffset modificationTime1 = await noteSecret.GetModificationTimeAsync(derivedKey, securityAsyncFunctions);
+			Thread.Sleep(1100);
+			await noteSecret.SetNoteTitleAsync("1234567", derivedKey, securityAsyncFunctions);
+			DateTimeOffset modificationTime2 = await noteSecret.GetModificationTimeAsync(derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.Greater(modificationTime2, modificationTime1);
+		}
+
+		[Test]
+		public async Task GetKeyIdentifierAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 111, 222, 31, 47, 75, 168, 78, 83, 91, 110, 221, 18, 213, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xa0, 0xb1, 0xcb, 0xcd, 0xaa, 0xc5, 0xd3, 0xb5, 0x58, 0x59, 0x15, 0x2b, 0x33, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 256, settingsAES_CTR);
+
+			string keyIdentifier = "primary";
+
+			string title = "Wishlist for holidays, eh";
+			string text = "peace, happiness, freedom, faster";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			// Act
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, keyIdentifier, skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.AreEqual(keyIdentifier, noteSecret.GetKeyIdentifier());
+		}
+
+		[Test]
+		public async Task CanBeDecryptedWithDerivedPasswordAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey1 = new byte[16] { 11, 222, 31, 47, 75, 168, 78, 13, 61, 118, 221, 18, 213, 104, 15, 16 };
+			byte[] derivedKey2 = new byte[16] { 11, 222, 31, 47, 75, 168, 78, 13, 61, 118, 221, 18, 213, 104, 15, 15 };
+			byte[] initialCounter = new byte[] { 0xa7, 0xb1, 0xcb, 0xcd, 0xaa, 0xc5, 0xd3, 0xb5, 0x58, 0x51, 0x95, 0x2b, 0x33, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 256, settingsAES_CTR);
+
+			string keyIdentifier = "primary";
+
+			string title = "Wishlist for holidays, eh";
+			string text = "peace, happiness, freedom, faster";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			// Act
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, keyIdentifier, skaAES_CTR, derivedKey1, securityAsyncFunctions);
+
+			// Assert
+			Assert.IsTrue(await noteSecret.CanBeDecryptedWithDerivedPasswordAsync(derivedKey1, securityAsyncFunctions));
+			Assert.IsFalse(await noteSecret.CanBeDecryptedWithDerivedPasswordAsync(null, securityAsyncFunctions));
+			Assert.IsFalse(await noteSecret.CanBeDecryptedWithDerivedPasswordAsync(new byte[] {}, securityAsyncFunctions));
+			Assert.IsFalse(await noteSecret.CanBeDecryptedWithDerivedPasswordAsync(derivedKey2, securityAsyncFunctions));
+		}
+
+		[Test]
+		public async Task SetNoteTitleAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 111, 222, 31, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+
+			SymmetricKeyAlgorithm ska = SymmetricKeyAlgorithm.GenerateNew(SymmetricEncryptionAlgorithm.AES_CTR, securityAsyncFunctions);
+
+			string title = "Wishlist for holidays part IV";
+			string text = "peace, happiness, freedom and something...";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", ska, derivedKey, securityAsyncFunctions);
+
+			string noteTitle1 = "future text that is happy and joyful for all holiday purposes...";
+
+			// Act
+			bool shouldBeTrue = await noteSecret.SetNoteTitleAsync(noteTitle1, derivedKey, securityAsyncFunctions);
+			string noteTitle2 = await noteSecret.GetNoteTitleAsync(derivedKey, securityAsyncFunctions);
+			bool shouldBeFalse = await noteSecret.SetNoteTitleAsync(noteTitle1,  new byte[] { 1, 2, 3 }, securityAsyncFunctions);
+
+			// Assert
+			Assert.IsTrue(shouldBeTrue);
+			Assert.IsFalse(shouldBeFalse);
+			Assert.IsFalse(string.IsNullOrEmpty(noteTitle2));
+			Assert.AreEqual(noteTitle1, noteTitle2);
+		}
+
+		[Test]
+		public async Task SetNoteTextAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 111, 222, 31, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+
+			SymmetricKeyAlgorithm ska = SymmetricKeyAlgorithm.GenerateNew(SymmetricEncryptionAlgorithm.AES_CTR, securityAsyncFunctions);
+
+			string title = "Top three";
+			string text = "Another long and super boring text for testing purposes.";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", ska, derivedKey, securityAsyncFunctions);
+
+			string noteText1 = "Not so boring but still bit something text for test case";
+
+			// Act
+			bool shouldBeTrue = await noteSecret.SetNoteTextAsync(noteText1, derivedKey, securityAsyncFunctions);
+			string noteText2 = await noteSecret.GetNoteTextAsync(derivedKey, securityAsyncFunctions);
+			bool shouldBeFalse = await noteSecret.SetNoteTextAsync(noteText1,  new byte[] { 1, 2, 3 }, securityAsyncFunctions);
+
+			// Assert
+			Assert.IsTrue(shouldBeTrue);
+			Assert.IsFalse(shouldBeFalse);
+			Assert.IsFalse(string.IsNullOrEmpty(noteText2));
+			Assert.AreEqual(noteText1, noteText2);
+		}
+
+		[Test]
+		public async Task ChecksumSurvivesRoundtripAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 15, 200, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 104, 15, 16 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xfb, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xf3, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for holidays";
+			string text = "peace, happiness, freedom";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret1 = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			string checksum1 = noteSecret1.GetChecksumAsHex();
+
+			string json = JsonConvert.SerializeObject(noteSecret1, Formatting.Indented);
+
+			NoteSecret noteSecret2 = JsonConvert.DeserializeObject<NoteSecret>(json);
+
+			// Assert
+			Assert.AreEqual(64, checksum1.Length);
+			Assert.AreEqual(checksum1, noteSecret2.GetChecksumAsHex());
+		}
+	}
+}
+
+#endif // ASYNC_WITH_CUSTOM
