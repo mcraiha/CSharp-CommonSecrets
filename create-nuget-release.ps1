c4f8a42f39f83a7561d0bad4e031546199b12a0e
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-03-31 18:56:01 +0300

    Rename build command for sync
    Since it now needs .csproj

--- create-nuget-release.ps1
+++
@@ -1,4 +0,0 @@
-$currentDate = Get-Date
-$gitShortHash = ((git rev-parse --short HEAD) | Out-String).Trim()
-$finalCommand = "dotnet pack" + " " + "--configuration Release" + " " + "--include-source" + " " + "--include-symbols" + " " + "/p:InformationalVersion=""" + $currentDate.ToUniversalTime().ToString("yyyy-MM-dd HH.mm.ss") + " Short hash: " + $gitShortHash + """"
-Write-Host $finalCommand
