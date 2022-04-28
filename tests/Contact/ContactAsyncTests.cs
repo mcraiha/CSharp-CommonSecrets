ed01f49e9132d5a4430a8ab958370090e5d94f1a
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-04-28 20:05:02 +0300

    Add PhoneNumbersAndDescriptionsOverload test case
    For better coverage

--- tests/Contact/ContactAsyncTests.cs
+++ tests/Contact/ContactAsyncTests.cs
@@ -261,6 +261,31 @@ namespace Tests
 			CollectionAssert.AreEqual(c1.GetEmailDescriptionsArray(), c2.GetEmailDescriptionsArray());
 		}
 
+		[Test, Description("Make sure both overloads work identically")]
+		public async Task UpdatePhoneNumbersAndDescriptionsOverloadAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			Contact c1 = await Contact.CreateContactAsync("", "", "", securityAsyncFunctions);
+			Contact c2 = await Contact.CreateContactAsync("", "", "", securityAsyncFunctions);
+
+			string[] phoneNumbersArray = { "1234-123-123", "2344-234-234" };
+			string phoneNumbers = string.Join(Contact.separatorString, phoneNumbersArray);
+
+			string[] phoneNumberDescriptionsArray = { "work", "hotel" };
+			string phoneNumberDescriptions = string.Join(Contact.separatorString, phoneNumberDescriptionsArray);
+
+			// Act
+			await c1.UpdatePhoneNumbersAndDescriptionsAsync(phoneNumbersArray, phoneNumberDescriptionsArray, securityAsyncFunctions);
+			await c2.UpdatePhoneNumbersAndDescriptionsAsync(phoneNumbers, phoneNumberDescriptions, securityAsyncFunctions);
+
+			// Assert
+			Assert.AreEqual(phoneNumbersArray.Length, c1.GetPhoneNumbersArray().Length);
+			CollectionAssert.AreEqual(c1.GetPhoneNumbersArray(), c2.GetPhoneNumbersArray());
+			CollectionAssert.AreEqual(c1.GetPhoneNumberDescriptionsArray(), c2.GetPhoneNumberDescriptionsArray());
+		}
+
 		[Test]
 		public async Task ShallowCopyAsyncTest()
 		{
