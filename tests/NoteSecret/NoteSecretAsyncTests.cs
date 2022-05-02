98e48979bdf7a4f987a447503e835606ef5e04e6
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-05-02 21:03:53 +0300

    Add GetCreationTimeAsyncTest tor NoteSecretAsyncTests
    Better coverage

--- tests/NoteSecret/NoteSecretAsyncTests.cs
+++ tests/NoteSecret/NoteSecretAsyncTests.cs
@@ -190,6 +190,33 @@ namespace Tests
 		}
 
 		[Test]
+		public async Task GetCreationTimeAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 255 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter, securityAsyncFunctions);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			string title = "Wishlist for async testing";
+			string text = "peace, happiness, freedom, await";
+
+			Note note = await Note.CreateNoteAsync(title, text, securityAsyncFunctions);
+
+			NoteSecret noteSecret = await NoteSecret.CreateNoteSecretAsync(note, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			DateTimeOffset noteCreationTime = await noteSecret.GetCreationTimeAsync(derivedKey, securityAsyncFunctions);
+
+			// Assert
+			Assert.AreEqual(note.GetCreationTime(), noteCreationTime);
+		}
+
+		[Test]
 		public async Task GetModificationTimeAsyncTest()
 		{
 			// Arrange
