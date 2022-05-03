3e61a952ac83e766d6460ac9f3bc33f9e357c340
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-05-03 21:05:08 +0300

    Add some test cases for invalid SymmetricKeyAlgorithm values
    Since those are easy to test

--- tests/SymmetricKeyAlgorithmSyncTests.cs
+++ tests/SymmetricKeyAlgorithmSyncTests.cs
@@ -20,6 +20,26 @@ namespace Tests
 		private const int validChaCha20KeyLength = 32;
 
 		[Test]
+		public void InvalidValuesTest()
+		{
+			// Arrange
+			object nullSettings = null;
+			int invalidKeySizeInBits = 53;
+
+			SettingsChaCha20 settingsChaCha20 = new SettingsChaCha20(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00 }, 1);
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff });
+
+			// Act
+
+			// Assert
+			Assert.Throws<ArgumentNullException>(() => new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.ChaCha20, 256, nullSettings));
+			Assert.Throws<ArgumentNullException>(() => new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 256, nullSettings));
+
+			Assert.Throws<ArgumentException>(() => new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.ChaCha20, invalidKeySizeInBits, settingsChaCha20));
+			Assert.Throws<ArgumentException>(() => new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, invalidKeySizeInBits, settingsAES_CTR));
+		}
+
+		[Test]
 		public void ChaCha20Test()
 		{
 			// Arrange
