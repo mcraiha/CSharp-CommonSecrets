5d15ab21bb3dc4ac83a41813e1b7a69ad4056917
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-05-03 21:09:16 +0300

    Add missing CheckIfChecksumMatchesContent test case
    For PaymentCardSecret

--- tests/PaymentCardSecret/PaymentCardSecretAsyncTests.cs
+++ tests/PaymentCardSecret/PaymentCardSecretAsyncTests.cs
@@ -687,6 +687,33 @@ namespace Tests
 			Assert.AreEqual(64, checksum1.Length);
 			Assert.AreEqual(checksum1, paymentCardSecret2.GetChecksumAsHex());
 		}
+
+		[Test]
+		public async Task CheckIfChecksumMatchesContentAsyncTest()
+		{
+			// Arrange
+			ISecurityAsyncFunctions securityAsyncFunctions = new SecurityAsyncFunctions();
+
+			byte[] derivedKey = new byte[16] { 56, 2, 3, 4, 55, 76, 7, 8, 9, 10, 11, 12, 13, 14, 15, 255 };
+			byte[] initialCounter = new byte[] { 0x00, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter, securityAsyncFunctions);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			PaymentCard paymentCard = await PaymentCard.CreatePaymentCardAsync("Bank of Dragon III", "Coolah Dragon", "Debit", "0000000000001234", "111", "11/20", "05/33", "Super cool card I have here", securityAsyncFunctions);
+
+			PaymentCardSecret paymentCardSecret = await PaymentCardSecret.CreatePaymentCardSecretAsync(paymentCard, "does not matter", skaAES_CTR, derivedKey, securityAsyncFunctions);
+
+			// Act
+			bool shouldBeTrue = await paymentCardSecret.CheckIfChecksumMatchesContentAsync(securityAsyncFunctions);
+			paymentCardSecret.checksum = paymentCardSecret.checksum.Remove(0, 1);
+			bool shouldBeFalse = await paymentCardSecret.CheckIfChecksumMatchesContentAsync(securityAsyncFunctions);
+
+			// Assert
+			Assert.IsTrue(shouldBeTrue);
+			Assert.IsFalse(shouldBeFalse);
+		}
 	}
 }
 
