833b7ec6a727211388eb7b95bd423f622aa99213
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2022-05-04 20:52:34 +0300

    Add GetCreationTimeTest
    For PaymentCardSecret

--- tests/PaymentCardSecret/PaymentCardSecretSyncTests.cs
+++ tests/PaymentCardSecret/PaymentCardSecretSyncTests.cs
@@ -304,6 +304,28 @@ namespace Tests
 		}
 
 		[Test]
+		public void GetCreationTimeTest()
+		{
+			// Arrange
+			byte[] derivedKey = new byte[16] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 255 };
+			byte[] initialCounter = new byte[] { 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff };
+
+			SettingsAES_CTR settingsAES_CTR = new SettingsAES_CTR(initialCounter);
+
+			SymmetricKeyAlgorithm skaAES_CTR = new SymmetricKeyAlgorithm(SymmetricEncryptionAlgorithm.AES_CTR, 128, settingsAES_CTR);
+
+			PaymentCard paymentCard = new PaymentCard("Bank of Dragon", "Coolest Dragon", "Credit", "0000000000001234", "111", "11/20", "05/33", "Super cool card I have here");
+
+			PaymentCardSecret paymentCardSecret = new PaymentCardSecret(paymentCard, "does not matter", skaAES_CTR, derivedKey);
+
+			// Act
+			DateTimeOffset paymentCardSecretCreationTime = paymentCardSecret.GetCreationTime(derivedKey);
+
+			// Assert
+			Assert.AreEqual(paymentCard.GetCreationTime(), paymentCardSecretCreationTime);
+		}
+
+		[Test]
 		public void GetModificationTimeTest()
 		{
 			// Arrange
